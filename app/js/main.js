2bd482019c5af46aa84b2925ded36323cddd8873
DmitryPashkou <86959216+DmitryPash@users.noreply.github.com>
2022-03-16 20:08:32 +0100

    fixiki
    fixiki

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,14 @@
 
 $(document).ready(function(){
    $('.top-screen-slider').slick({
+      autuHeight: true,
       infinite: true,
       prevArrow: $('.prev'),
       nextArrow: $('.next'),
    });
  });
 
- 
-// const topScreen= document.querySelector(".top-screen").classList.contains("top-screen");
-// const HeaderStyle = document.querySelectorAll('.header-info-link');
 
-// if (topScreen == true) {
-//    HeaderStyle.forEach(element => {
-//       element.style.color = 'red'
-//    });
-// }
-// console.log(topScreen)
 let menu = document.querySelectorAll(".footer-info");
 menu.forEach((element) => {
   element.addEventListener("click", () => {
