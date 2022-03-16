d16663591c10ec0b60b9f245bfdeef40b6f833a9
DmitryPashkou <86959216+DmitryPash@users.noreply.github.com>
2022-03-16 14:49:53 +0100

    zxc
    zxc

---
+++ app/js/main.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+   $('.top-screen-slider').slick({
+      infinite: true,
+      prevArrow: $('.prev'),
+      nextArrow: $('.next'),
+   });
+ });
+
+ 
+// const topScreen= document.querySelector(".top-screen").classList.contains("top-screen");
+// const HeaderStyle = document.querySelectorAll('.header-info-link');
+
+// if (topScreen == true) {
+//    HeaderStyle.forEach(element => {
+//       element.style.color = 'red'
+//    });
+// }
+// console.log(topScreen)
