9b8da2e3641a02c9cd0c60d10eb7304169b0dab1
DmitryPashkou <86959216+DmitryPash@users.noreply.github.com>
2022-03-20 15:27:27 +0100

    zxc
    zxc

--- app/js/main.js
+++ app/js/main.js
@@ -25,10 +25,38 @@ $(document).ready(function () {
   });
 });
 
-//
+//Footer drop
 let menu = document.querySelectorAll(".footer-info");
 menu.forEach((element) => {
   element.addEventListener("click", () => {
     element.classList.toggle("open");
   });
 });
+
+//Change color for Header
+
+let headerLogo = document.getElementById("header_logo");
+let headerShop = document.getElementById("header_shop");
+let headerSearch = document.getElementById("header_search");
+let headermenu = document.querySelectorAll(".header-info-link-title");
+let arrow = document.querySelectorAll(".arrow");
+
+let pastName = window.location.pathname === "/main.html";
+
+function changeHeader() {
+  headerLogo.src = "./images/icon/ESPIREO-white.svg";
+  headerShop.src = "./images/icon/header-shop-white.svg";
+  headerSearch.src = "./images/icon/header-search-white.svg";
+  headermenu.forEach((el) => {
+    el.style.color = "#fff";
+  });
+  arrow.forEach((el) => {
+    el.style.borderColor = "#FFF";
+  });
+}
+
+console.log(headermenu);
+
+if (pastName == true) {
+  changeHeader();
+}
