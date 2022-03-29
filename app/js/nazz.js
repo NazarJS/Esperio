64fe71a30ade226868d5acb6801e95e1a8fb8fe1
NazarJS <nazar.poluyanow@gmail.com>
2022-03-29 17:01:43 +0300

    fix trello

--- app/js/nazz.js
+++ app/js/nazz.js
@@ -6,20 +6,25 @@ headerInputMob.onclick  = function(e) {
     e.stopPropagation();
     inputListMob.style.display = "block";
 }
-    
-wrapper.onclick = () => {
-    inputList.style.display = "none";
-    inputListMob.style.display = "none";
-}
-magnifier.onclick = () => {
+magnifier.onclick = (e) => {
+    e.stopPropagation();
     magnifier.style.display = "none";
     headerBtnsSearch.style.display = "inline-block";
-}
-//hide input
-magnifierInput.onclick = () => {
-    magnifier.style.display = "inline-block";
+}    
+wrapper.onclick = (e) => {
+    e.stopPropagation();
+    // inputList.style.display = "none";
+    inputListMob.style.display = "none";
     headerBtnsSearch.style.display = "none";
+    inputList.style.display = "none";
+    magnifier.style.display = "inline-block";
 }
+
+//hide input
+// magnifierInput.onclick = () => {
+//     magnifier.style.display = "inline-block";
+//     headerBtnsSearch.style.display = "none";
+// }
 burger.onclick = () => {
     headerDesctop.style.display = "none";
     headerMob.style.display = "block";
