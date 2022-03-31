bc1136fd9a9a66a28a6d6908d7ac1df4faca10a2
arichild <arieevq@gmail.com>
2022-03-31 17:18:16 +0300

    add slides using mouse scroll

--- dist/js/arina.js
+++ dist/js/arina.js
@@ -1,6 +1,7 @@
 const product = document.querySelector('.product-info-block');
 const preview = document.querySelector('.product-img');
 const img = document.querySelectorAll('.img-gallery');
+const slider = $(".product-items");
 
 // product slider 
 for (let i = 0; i < img.length; i++) {
@@ -15,11 +16,24 @@ for (let i = 0; i < img.length; i++) {
 // slick slider
 $(document).ready(function () {
   $('.product-items').slick({
-    arrows: false,
+    dots: true,
+    prevArrow: false,
+    nextArrow: false,
     vertical: true,
     verticalSwiping: true,
   }); 
 });
+
+// Implementing navigation of slides using mouse scroll
+slider.on('wheel', (function(e) {
+  e.preventDefault();
+
+  if(e.originalEvent.deltaY < 0) {
+    $(this).slick('slickNext');
+  } else {
+    $(this).slick('slickPrev');
+  }
+}));
 
 // off jQuery form styler in product.html
 if(product) {
